fb06da5f11afc1228e9b44186f77e21431b78a6a
itsadech <tamama_kung@hotmail.com>
2013-09-04 09:41:25 +0700

    โค๊ดเว็บ

---
+++ add.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="80%" border="1">
+  <tr>
+    <td colspan="2">กรอกข้อมูล admin</td>
+  </tr>
+  <tr>
+    <td>หน้าหลัก</td>
+    <td>ติดต่อสอบถาม</td>
+  </tr>
+  <tr>
+    <td colspan="2"><form id="form1" name="form1" method="post" action="">
+      <p>
+        <label for="textfield">รหัส ดอกไม้</label>
+        <input type="text" name="textfield" id="textfield" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <label for="textfield7">image picture</label>
+        <input type="text" name="textfield7" id="textfield7" />
+        <input type="submit" name="button2" id="button2" value="OK" />
+      </p>
+      <p>
+        <label for="textfield2">ชื่อดอกไม้</label>
+        &nbsp;&nbsp;
+        <input type="text" name="textfield2" id="textfield2" />
+      </p>
+      <p>
+        <label for="textfield3">สายพันธ์</label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="textfield3" id="textfield3" />
+        </p>
+      <p>
+        <label for="textfield4"> จำนวน&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </label>
+        <input type="text" name="textfield4" id="textfield4" />
+      </p>
+      <p>
+        <label for="textfield5">ราคา</label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input type="text" name="textfield5" id="textfield5" />
+      </p>
+      <p>
+        <label for="textfield6">วันและเวลลา</label>
+        <input type="text" name="textfield6" id="textfield6" />
+      </p>
+      <p>&nbsp;</p>
+       <input type="submit" name="button" id="button" value="ADD" />      <input type="submit" name="button" id="button" value="DELETE" />
+      <p>
+        <input type="submit" name="button" id="button" value="OK" />
+        <input type="submit" name="button3" id="button3" value="cancel" />
+      </p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+    </form>
+      <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
